43b819c06e10fde2ed1a5097df74d0501f86699a
muxammadziz <muxammadazizramziddinov@gmail.com>
2022-07-24 13:25:43 +0500

    changet Port

--- src/server.js
+++ src/server.js
@@ -21,6 +21,6 @@ app.use((error, req, res) => {
     return res.send({ error: error.error?.message || "somethink went wrong"})
 })
 
-app.listen(5000 || process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('server is run');
 })
